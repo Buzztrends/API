2c22eb2be95ffea68fc716652e487fbc93396ca3
zee-1 <zaid.khan21800@gmail.com>
2023-11-07 23:33:49 +0530

    Feature: MongoDB connectivity and Create API subscriber

--- server.py
+++ server.py
@@ -1,13 +1,11 @@
 #------------- SERVER IMPORTS-------------
 import os
 import json
-import uuid
 import base64
 from functools import wraps
-from hmac import compare_digest
-from flask import Flask, request,make_response
-from rsa import newkeys, decrypt, encrypt,PrivateKey
-from flask_pymongo import PyMongo
+from flask import Flask, request
+from rsa import newkeys, decrypt, encrypt,PrivateKey,PublicKey
+from flask_pymongo import MongoClient
 #------------- MODULES IMMPORTS-----------
 
 # Image Generation Module
@@ -21,6 +19,9 @@ from TextGeneration.ReferencePostGeneration import *
 # Moments Module
 from Moments.Moments import *
 #===========Config Setup==================
+
+
+
 if not os.path.exists("./config/.env"):
     os.mkdir("./config")
     with open("./config/.env","w") as f:
@@ -32,29 +33,21 @@ if not os.path.exists("./config/users"):
         f.write("{}")
 
 #==================================================================================================
+
+#===========Models=========================
+
+from models import APIModel
+
+#==========================================
+
+#==============App Setup==================
 app = Flask("BuzztrendsAPI")
+db = MongoClient(os.environ["MONGO_URI"])
+#=========================================
 
-
-#=========== Models ========================
-class ApiUsers:
-    def __init__(self,user:str,role:str) -> None:
-        self.id = str(uuid.uuid4())
-        self.user = user
-        self.role = role
-        self.public_key = None
-    
-    def from_dict(self,userData:dict):
-        self.id = userData.keys()[0]
-        self.user = userData[self.id]["user"]
-        self.role = userData[self.id]["role"]
-
-    def to_dict(self):
-        return {self.id:{"user":self.user,"role":self.role}}
-
-    def set_public_key(self,key):
-        self.public_key = key
-
+#================ Authorization ==========
 def admin_action(func):
+    global db
     @wraps(func)
     def decor(*args,**kwargs):
         key = None
@@ -83,22 +76,66 @@ def admin_action(func):
         except Exception as e:
             print(e)
             return json.dumps({"message":"Invalid ID detected","status_code":401})
-        with open("./config/users","r") as f:
-            users = eval(f.read())
-            if userId not in users:
-                return json.dumps(dict(message="Invalid User Provided",status_code=401))
-            else:
-                if users[userId]["role"]!= "admin":
-                    return json.dumps(dict(message="User is not admin",status_code=401))
+        collection = db["users"]["api_subscribers"]
+        print(userId)
+        db_resp= collection.find_one({"uid":userId})
+        print(db_resp,"\n================\n")
+        if db_resp is None:
+            return json.dumps(dict(message="Invalid User Provided",status_code=401))
+        else:
+            if db_resp["role"]!= "admin":
+                return json.dumps(dict(message="User is not admin",status_code=401))
         return func()
     return decor 
+
+def auth_api_key(func):
+    @wraps(func)
+    def decor(*args,**kwargs):
+        global db
+        key = None
+        try:
+            print("\nHeader:",request.headers)
+            print("\nJSON:",request.get_json())
+        except Exception as e:
+            print(e)
+
+        if "api-key" in request.headers:
+            key = (request.headers["api-key"])
+            decode_key = base64.b64decode(key.encode("utf-8"))    
+            print("Key Captured",key)
+
+        if not key:
+            print(request.get_json())
+            return json.dumps({"message":"Admin Action Triggered! No admin Key found."})
+
+
+        with open("./config/.env","r") as f:
+            data = eval(f.read())
+            if "PRIVATE_KEY" not in data:
+                return json.dumps({"message":"No admin Registered Create an admin first","status_code":401})
+            private_key_b64 = data["PRIVATE_KEY"]
+            private_key = base64.b64decode(private_key_b64)
+            private_key = PrivateKey.load_pkcs1(private_key)
+        try:
+            userId = decrypt(decode_key,priv_key=private_key).decode()
+        except Exception as e:
+            print(e)
+            return json.dumps({"message":"Invalid ID detected","status_code":401})
+        else:
+            if db["users"]["api_subscribers"].find_one({"uid":userId}) is None:
+                return json.dumps({"message":"Invalid ID detected","status_code":401})
+            else:
+                return func()
+    return decor
+
+
 @app.route("/create_admin")
 def create_admin():
 
 
-    user = Users("admin","admin")
+    user = APIModel("admin")
+    user.role = "admin"
     pub, priv = newkeys(512)
-    user.set_public_key(pub)
     with open("./config/.env","r") as f:
         config = eval(f.read())
         if "PRIVATE_KEY" in config:
@@ -114,15 +151,11 @@ def create_admin():
         config["PUBLIC_KEY"] =pub_b64
     with open("./config/.env","w") as f:
         f.write(json.dumps(config))
-    with open("./config/users","r+") as f:
-        users = eval(f.read())
-        users[user.id] = user.to_dict()[user.id]
-    with open("./config/users","w") as f:
-        
-        f.write(json.dumps(users))
     
-    API_KEY = encrypt(user.id.encode(),pub)
+    API_KEY = encrypt(user.uid.encode(),pub)
     API_KEY = base64.b64encode(API_KEY).decode("utf-8")
+    print(user.to_json())
+    db["users"]["api_subscribers"].insert_one((user.to_json()))
     return json.dumps(
         dict(
             message="Admin Created Successfully!",
@@ -141,21 +174,33 @@ def register():
         print(e)
         return json.dumps(dict(message="Registration Unsuccessful"))
     else:
-        user = ApiUsers(data["user"],role="non_admin")
-        with open("./config/users","r") as f:
-            users_data = eval(f.read())
-        users_data[user.id] = user.to_dict()[user.id]
-        with open("./config/users","w") as f:
-            f.write(json.dumps(users_data))
-        return json.dumps(dict(message="Registration Successful"))
+        user = APIModel(data["user"])
+        with open("./config/.env","r") as f:
+            config = eval(f.read())
+            pub_b64 = config["PUBLIC_KEY"]
+        pub = base64.b64decode(pub_b64)
+        pub = PublicKey.load_pkcs1(pub)
+        API_KEY = encrypt(user.uid.encode(),pub)
+        API_KEY = base64.b64encode(API_KEY).decode("utf-8")
+        print(user.to_json())
+        db["users"]["api_subscribers"].insert_one((user.to_json()))
+        return json.dumps(
+            dict(
+                message="User Registered Successfully!",
+                API_KEY=API_KEY,
+                status_code=200
+            )
+        )  
+
 @app.route("/test/users")
 def list_users():
-    pass
+    db["api_subscribers"]["users"]
 @app.route("/",methods=["GET"])
 def index_():
     return "BuzzTrends API"
 
 #           Image Generation Route
+@auth_api_key
 @app.route("/image_generation")
 def generate_image():
     # write the driver code here
@@ -167,6 +212,7 @@ def generate_image():
     )
 
 #           Text Generation Route - Simple Generation
+@auth_api_key
 @app.route("/text_generation/simple_generation")
 def generate_post():
     # write the driver code here
@@ -178,6 +224,7 @@ def generate_post():
     )
 
 #           Text Generation Route - Reference Post Generation
+@auth_api_key
 @app.route("/text_generation/reference_post_generation")
 def generate_reference_post():
     # write the driver code here
@@ -189,6 +236,7 @@ def generate_reference_post():
     )
 
 #           Text Generation Route - Catelogue Generation
+@auth_api_key 
 @app.route("/text_generation/catelogue_generation")
 def generate_post_from_catalogue():
     # write the driver code here
