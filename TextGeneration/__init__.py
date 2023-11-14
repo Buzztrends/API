0ec086218e3f50b1b33e9bd9ca731cc925cd3071
zee-1 <zaid.khan21800@gmail.com>
2023-11-14 11:45:36 +0530

    Feature: TextGeneration/CatelogueGeneration

--- TextGeneration/__init__.py
+++ TextGeneration/__init__.py
@@ -1,2 +1,4 @@
-from .utils import get_llm
+from .utils import get_llm, prepare_prods
 from .SimpleGeneration import generate_content
+from .ReferencePostGeneration import generate_similar_content
+from .CatelogueGeneration import *
