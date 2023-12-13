f7b8a01388988ff2f9bd94e4c9d7a595f86c05a8
zee-1 <zaid.khan21800@gmail.com>
2023-12-13 21:45:58 +0530

    Loggers: Moment and Content Gen Loggers

--- TextGeneration/CatelogueGeneration.py
+++ TextGeneration/CatelogueGeneration.py
@@ -1,8 +1,11 @@
-from .SimpleGeneration import generate_content
+from .SimpleGeneration import generate_content,generate_content_2
 from .ReferencePostGeneration import generate_similar_content
 from .utils import prepare_prods
 from typing import Union
 import pandas as pd
+from logger.contentGenLogger import ContentGenLogger
+
+logger = ContentGenLogger().getLogger()
 
 def generate_post_with_prod(company_name: str,
     moment: str,
@@ -14,6 +17,7 @@ def generate_post_with_prod(company_name: str,
     moment_retriver,
     product_name:str,
     products:dict,
+    extras_guidelines:str,
     ref_post:Union[str,None] = None,
     tone:Union[str,None] = None,
     structure:Union[str,None] = None,
@@ -21,9 +25,20 @@ def generate_post_with_prod(company_name: str,
     )-> dict[str,str]:
     prod = prepare_prods(products[product_name],product_name)
     company_info += "Following are some of the products sell by {company_name}. Talk about the following products in the post: \n"+ prod
-
+    logger.info(f"{__name__}:\n- Parameters:\\
+                 \n\tCompany Name:{company_name}\
+                 \n\t-Moment:{moment}\
+                 \n\t-Content Type:{content_type}\
+                 \n\t-Tone:{tone}\
+                 \n\t-Objective:{objective}\
+                 \n\t-Structure:{structure}\
+                 \n\t-Location:{location}\
+                 \n\t-Audience:{audience}\
+                 \n\t-Company Info:{company_info}\
+                 \n\t-Extras Guidelines:{extras_guidelines}")
     if ref_post is None:
-        return generate_content(
+        logger.info("Hitting the generate_content_2 method")
+        return generate_content_2(
                         company_name,
                         moment,
                         content_type,
@@ -34,9 +49,11 @@ def generate_post_with_prod(company_name: str,
                         audience,
                         company_info,
                         moment_retriver,
-                        model="gpt_4_high_temp"
+                        extras_guidelines,
+                        model="gpt_4_high_temp",
                     )
     else:
+        logger.info("Hitting the generate_similar_content method")
         return generate_similar_content(company_name,
                 moment,
                 content_type,
@@ -46,5 +63,6 @@ def generate_post_with_prod(company_name: str,
                 company_info,
                 moment_retriver,
                 ref_post,
-                model="gpt_4_high_temp"
+                model="gpt_4_high_temp",
+                extras_guidelines=extras_guidelines,
             )
